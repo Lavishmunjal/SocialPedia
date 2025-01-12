c1bd6faacb6b1d2ee16b352b6bda7ed60fdf223e
Lavishmunjal <0111lavishmunjal@gmail.com>
2025-01-12 10:26:16 +0530

    Authentication and Authorization done

---
+++ server/routes/auth.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { Router } from 'express'
+import {login} from '../controllers/auth.js'
+const router = Router()
+
+router.post("/login", login)
+
+export default router
